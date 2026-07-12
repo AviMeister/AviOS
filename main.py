f2bb236789e3cc1a14a55a4665e53d0315c89897
Avi Chetram <avimichaelmeister@gmail.com>
2026-07-12 20:20:56 +0200

    AviOS first run - welcome message working

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-tasks = [] ()
-
+# AviOS: This is where AviOS starts. Light intro, tone, art/visual
+# Will adjust intro text later maybe with stylings
+# Greet the user when AviOS(software) launches
+print ("AviOS is starting up. Let's build something real.")
